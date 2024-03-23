4a085825c142cc0427f7ff91610690a9b644db1f
anjaligiri7777 <anjaligiri7007@gmail.com>
2024-03-23 19:24:09 +0530

    Anjali_007:BASE CLASS UPDATED

--- src/main/java/com/BaseClasses/BaseClass.java
+++ src/main/java/com/BaseClasses/BaseClass.java
@@ -5,6 +5,6 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("HELLO ");
-		System.out.println("updated project at 6:20");
+		System.out.println("updated project at 07:22");
 	}
 }
